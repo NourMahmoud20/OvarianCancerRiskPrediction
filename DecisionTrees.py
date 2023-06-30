605a197bafbe572d48771b0f6812ffafe8de359c
NourMahmoud20 <59208607+NourMahmoud20@users.noreply.github.com>
2023-06-30 12:59:51 -0400

    Update DecisionTrees.py

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -29,28 +29,7 @@ dataPLCO = pd.read_csv('/content/drive/My Drive/PLCO.csv')
 
 
 #Inputs: Family History (Ovarian and Breast cancer), Body Type (BMI, Weight,Height), NSAIDS (Aspirin and Ibuprofen intake), Diseases (Arthritis, Hypertension,Diabetes, Osteoporosis, and Colorectal Polyps), and Smoking
-columns_to_drop =['ovar_reasfoll', 'ovar_reassurv', 'ovar_reassymp', 'ovar_reasoth', 'ovar_intstat_cat', 'ovar_annyr', 
-'ovar_cancer_site', 'ovar_stage', 'ovar_stage_7e', 'ovar_stage_t', 'ovar_stage_n', 'ovar_stage_m', 'ovar_clinstage', 
-'ovar_clinstage_7e', 'ovar_clinstage_t', 'ovar_clinstage_n', 'ovar_clinstage_m', 'ovar_pathstage', 'ovar_pathstage_7e', 
-'ovar_pathstage_t', 'ovar_pathstage_n', 'ovar_pathstage_m', 'ovar_grade', 'ovar_topography', 
-'ovar_morphology', 'ovar_behavior', 'ovar_histtype', 'ovar_exitstat', 'ovar_exitage', 'ovar_seer',
-'ovar_cancer_first', 'ovar_curative_surg', 'ovar_curative_chemo', 'ovar_primary_trt', 'ovar_num_heslide_imgs',
-'ovar_has_deliv_heslide_img', 'plco_id', 'build', 'build_cancers', 'build_incidence_cutoff', 'ovar_exitdays',
-'ovar_primary_trt_days', 'ovar_cancer_diagdays', 'biopolink0', 'biopolink1', 'biopolink2', 'biopolink3', 'biopolink4',
-'biopolink5', 'ovar_mra_stat0', 'ovar_mra_stat1', 'ovar_mra_stat2', 'ovar_mra_stat3', 'ovar_mra_stat4', 
-'ovar_mra_stat5', 'tvu_result0', 'tvu_result1', 'tvu_result2','ca125_result0', 'ca125_result1', 
-'ca125_result2', 'ca125_result3', 'ca125_result4', 'ca125_level0', 'ca125_level1', 'ca125_level2',
-'ca125_level3', 'ca125_level4', 'ca125_level5', 'ca125_src0', 'ca125_src1', 'ca125_src2', 'ca125_src3', 'ca125_src4',
-'ca125_src5', 'ca125ii_level0', 'ca125ii_level1', 'ca125ii_level2', 'ca125ii_level3', 'ca125ii_level4', 
-'ca125ii_level5', 'orem_fyro', 'ca125_prot', 'ca125_days0', 'ca125_days1', 'ca125_days2', 'ca125_days3', 
-'ca125_days4', 'ca125_days5', 'tvu_days0', 'tvu_days1', 'tvu_days2', 'tvu_days3','bq_adminm','bq_returned', 
-'bq_age','bq_compdays','d_dthovar','f_dthovar','d_codeath_cat', 'f_codeath_cat','d_cancersite', 
-'f_cancersite', 'd_seer_death', 'f_seer_death', 'is_dead_with_cod', 'is_dead', 'mortality_exitage', 'mortality_exitstat',
-'build_death_cutoff', 'dth_days', 'mortality_exitdays','entryage_bq', 'entryage_dqx', 'entryage_dhq', 'entryage_sqx', 
-'entryage_muq','ph_any_bq', 'ph_any_dqx', 'ph_any_dhq', 'ph_any_sqx', 'ph_any_muq', 'ph_ovar_bq', 'ph_ovar_dqx', 'ph_ovar_dhq',
-'ph_ovar_sqx', 'ph_ovar_muq','ovar_eligible_bq', 'ovar_eligible_sqx', 'ovar_eligible_dhq', 'ovar_eligible_dqx', 'entrydays_bq',
-'entrydays_dqx', 'entrydays_dhq', 'entrydays_sqx', 'entrydays_muq','fstcan_exitstat', 'fstcan_exitage', 'fstcan_exitdays',
-'in_TGWAS_population', 'reconsent_outcome', 'reconsent_outcome_days','dual','center','arm','rndyear'] 
+columns_to_drop =[''] 
 
 dataPLCO = dataPLCO.drop(columns=columns_to_drop)
 
@@ -60,7 +39,7 @@ dataPLCO = dataPLCO.dropna(subset=['ovar_cancer'])
 dataPLCOx=dataPLCO.drop(columns=['ovar_cancer'])
 
 #imputing numeric values with mean value
-dataPLCOx[['weight_f', 'height_f', 'breast_fh_age', 'breast_fh_cnt', 'ovarsumm_fh_age', 'ovarsumm_fh_cnt']] = dataPLCOx[['weight_f', 'height_f', 'breast_fh_age', 'breast_fh_cnt', 'ovarsumm_fh_age', 'ovarsumm_fh_cnt']].fillna(dataPLCOx[['weight_f', 'height_f', 'breast_fh_age', 'breast_fh_cnt', 'ovarsumm_fh_age', 'ovarsumm_fh_cnt']].mean())
+dataPLCOx[['']] = dataPLCOx[['']].fillna(dataPLCOx[['']].mean())
 
 # Imputing missing categorical values with mode
 imputer = SimpleImputer(missing_values=np.NaN, strategy='most_frequent')
